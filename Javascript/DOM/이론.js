476e7048552a853f253960747884f04c3a509b03
[SW2/서울/2반] 정덕준 <jungdukjoon@gmail.com>
2022-08-23 20:44:20 +0900

    20220823

---
+++ Javascript/DOM/이론.js
@@ -0,0 +1,25 @@
+// document.getElmentbyId(): 해당 아이디의 요소를 선택
+// document.getElementsByClassName(): 클래스... 
+
+// Html 요소의 생성
+// document.createElement(): 지정된 HTML 요소를 생성
+// document.write() 
+
+// HTML 이벤트 핸들러
+// 요소.onclick = function(){}: 마우스 클릭 이벤트와 연결될 이벤트 핸들러
+
+// DOM: 계층화된 구조를 표현
+
+
+// // HTML <li> 요소를 선택
+// var selectedItem = document.getElementsByTagName("li")
+
+// // 스타일 변경
+
+// var selectedItem = document.getElementById("even");
+
+// selectedItemstylecolor = "red";
+
+// var str = document.getElementById("text");
+
+// str innerHTML = "요소의 내용을 바꿉니다.";
